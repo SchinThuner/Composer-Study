03151d16899667bd18229641aecad10754e18ec7
Schindler <schin.thuner@gmail.com>
2024-02-21 17:33:19 -0300

    arquivo config para Phan

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,7 +1,9 @@
 <?php
+
 //  composer install -> instala as depedencias presentes no composer.lock
 //  composer update -> atualiza o composer.lock instalando oque tem no arquivo composer.json
 //  composer dump-autoload -> atualiza o autoload presente no composer.json para que o autoloader
+// vendor/bin/phan --allow-polyfill-parser
 //  tenha acesso ao caminho de uma nova classe por exemplo
 require 'vendor/autoload.php';
 // require 'src/Buscador.php';
@@ -12,8 +14,7 @@ use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client(['base_uri' => 'https://www.alura.com.br/']);
 $crawler = new Crawler();
-
-$buscador= new Buscador($client, $crawler);
+$buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 MensagemInicio::metodoInicio();
 foreach ($cursos as $curso) {
