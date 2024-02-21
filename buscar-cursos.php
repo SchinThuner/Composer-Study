68b8a7013658fa3151b12d7d7edd2850f43994db
Schindler <schin.thuner@gmail.com>
2024-02-21 14:00:48 -0300

    Incluido namespaces no autoload

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,22 +1,17 @@
 <?php
-
+//  composer install -> instala as depedencias presentes no composer.lock
+//  composer update -> atualiza o composer.lock instalando oque tem no arquivo composer.json
+//  composer dump-autoload -> atualiza o autoload presente no composer.json para que o autoloader
+//  tenha acesso ao caminho de uma nova classe por exemplo
 require 'vendor/autoload.php';
-require 'src/Buscador.php';
+// require 'src/Buscador.php';
 
-use Alura\BuscadorDeCursos\Buscador;
+use Schindlerth\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client(['base_uri' => 'https://www.alura.com.br/']);
 $crawler = new Crawler();
-// $resposta = $client->request('GET','/cursos-online-programacao/php');
-
-// $html = $resposta->getBody();
-
-// $crawler = new Crawler();
-// $crawler->addHtmlContent($html);
-
-// $cursos = $crawler->filter('span.card-curso__nome');
 
 $buscador= new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
--- src/Buscador.php
+++ src/Buscador.php
@@ -1,5 +1,6 @@
 <?php
-namespace Alura\BuscadorDeCursos;
+namespace Schindlerth\BuscadorDeCursos;
+
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
 
